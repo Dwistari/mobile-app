c2cf2de344a992a15063201aeed3b1fb50ab58f0
dwistari <dwi241296>
2017-10-06 09:59:46 +0700

    login

---
+++ Amikom.app/app/src/main/java/net/dwistari/amikomapp/MainActivity.java
@@ -0,0 +1,19 @@
+package net.dwistari.amikomapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    String msg = "--Amikom App :";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
